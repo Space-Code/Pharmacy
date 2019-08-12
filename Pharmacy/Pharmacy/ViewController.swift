b6ef6b498eb307332e5373c1e3749765509247fe
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2019-08-12 13:37:42 -0300

    Add Swift version

---
+++ Pharmacy/Pharmacy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pharmacy
+//
+//  Created by Rodrigo Santos on 12/08/19.
+//  Copyright © 2019 Rodrigo Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
